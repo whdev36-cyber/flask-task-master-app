2278e54e708e0dad77cc20475882a7423d5676ef
WCyber <whdev36@gmail.com>
2025-05-03 19:39:04 +0500

    feat: implement user login functionality with validation and redirection

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
 db = SQLAlchemy()
-# login_manager = LoginManager()
+login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__, template_folder='templates', static_folder='static',
@@ -13,10 +13,16 @@ def create_app():
     # Initialize extensions
     db.init_app(app) # Initialize SQLAlchemy
 
-    # login_manager.init_app(app) # Initialize Flask-Login
-    # login_manager.login_view = 'auth.login' # Set the login view for Flask-Login
-    # login_manager.login_message = 'Please log in to access this page.' # Set the login message for Flask-Login
-    # login_manager.login_message_category = 'info' # Set the login message category for Flask-login
+    login_manager.init_app(app) # Initialize Flask-Login
+    login_manager.login_view = 'auth.login' # Set the login view for Flask-Login
+    login_manager.login_message = 'Please log in to access this page.' # Set the login message for Flask-Login
+    login_manager.login_message_category = 'info' # Set the login message category for Flask-login
+
+    # User loader
+    from .models import User
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
 
     # Register blueprints
     from . import views, auth
--- app/auth.py
+++ app/auth.py
@@ -1,7 +1,24 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template as render, redirect, flash, url_for, request as req
+from  flask_login import current_user, login_user, logout_user, login_required
+from app.forms import LoginForm, UserCreationForm, TaskForm
+from app.models import User
+from werkzeug.security import generate_password_hash, check_password_hash
 
 bp = Blueprint('auth', __name__) # Create a blueprint for the authentication module
 
-@bp.route('/login')
+@bp.route('/login', methods=['POST', 'GET'])
 def login():
-    return render_template('login.html')
+    if current_user.is_authenticated:
+        return redirect(url_for('main.home'))
+
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            flash('You have successfully logged in!', category='success')
+            next_page = req.args.get('next')
+            return redirect(next_page or url_for('main.home'))
+        else:
+            flash('Invalid password or email, please try again!', category='error')
+    return render('login.html', form=form)
